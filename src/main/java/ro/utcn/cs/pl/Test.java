25e4966755736d8a10d478ed5db3a47ea9728bf3
Adrian Bona <adrianulbona@gmail.com>
2014-10-27 23:51:00 +0200

    added gradle support.

--- src/main/java/ro/utcn/cs/pl/Test.java
+++ src/main/java/ro/utcn/cs/pl/Test.java
@@ -2,7 +2,7 @@ package ro.utcn.cs.pl;
 
 public class Test {
     public static void main(String[] argv) {
-        final PrologEngine engine = PrologEngine.init("pl/logic.pl");
+        final PrologEngine engine = PrologEngine.init(argv[0]);
         engine.runQuery("middle([1, 2, 3, 4, 5, 6, 7, 8], M)");
     }
 }
